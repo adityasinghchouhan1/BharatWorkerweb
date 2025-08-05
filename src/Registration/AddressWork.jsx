b97df16faad50a2b724f00bef95a48836797dc94
adityasinghchouhan1 <asadityasingh2572000@gmail.com>
2025-08-05 18:25:13 +0530

    imp

---
+++ src/Registration/AddressWork.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import useRegistration from './RegistrationContext/useRegistration'
+
+const AddressWork = () => {
+  return <div>AddressWork</div>
+}
+
+export default AddressWork
